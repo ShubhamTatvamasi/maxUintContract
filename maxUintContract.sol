3900cd091641d1da65a33462f197ce9196dcd318
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2018-02-19 22:23:28 +0530

    Create maxUintContract.sol

---
+++ maxUintContract.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.20;
+
+contract maxUintContract {
+
+    // variable for storing the maximum uint value
+    uint public maxUint;
+
+    // constructor function
+    function maxUintContract() public {
+
+    // initialize thisContractAddress with the maximum uint value
+    maxUint = (2**256 - 1);
+
+    }
+    
+}
